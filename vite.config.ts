ea7a9992d0ff4805fa4826c83d8ec41870c44e79
Dammy <97961660+Dammydx@users.noreply.github.com>
2025-02-04 16:41:55 -0800

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Dammy-Tech-HUB/", // 👈 Replace with your GitHub repository name
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
